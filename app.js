8d3f25dbc8296ac44e3faf54ac88bfbcdc46f794
Marcel Champagne <champagn@grinnell.edu>
2017-07-03 16:40:10 -0500

    Add basic server

---
+++ app.js
@@ -0,0 +1,17 @@
+// Imports
+const express = require('express')
+var path = require('path');
+
+// Initialize Express
+const app = express()
+app.use(express.static('assets'))
+
+// Define routes
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+// Start sever
+app.listen(3000, function () {
+  console.log('Spotify manager listening on port 3000!')
+})
