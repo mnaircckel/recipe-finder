8cea6082c7d32cb67ef116d89207a3f6f52b4c78
Marcel Champagne <champagn@grinnell.edu>
2017-07-10 13:28:50 -0500

    Server functions for Spotify auth

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  clientId: process.env.SpotifyId,
+  clientSecret: process.env.SpotifySecret,
+  redirectUri: process.env.SpotifyRedirectUri,
+  stateKey: 'SPOTIFY_STATE_KEY',
+}
